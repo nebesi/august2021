23db6a226debbc59006a4706ed9b2ae67dcaf52f
max_shirokov <maksim.shirokov2011@mail.ru>
2021-08-19 21:33:19 +0300

    поменял шаблон под дальнейшую работу

--- js/main.js
+++ js/main.js
@@ -1,33 +1,46 @@
-const defaultImage = "https://santehmag.com.ua/image/cache/catalog/import_yml/SD0/003/724/6/no-img-200x200.jpg"
+class ProductList {
+    constructor(container = '.products') {
+        this.container = container;
+        this.goods = [];
+        this._fetchProducts();
+        this.render();//вывод товаров на страницу
+    }
+    _fetchProducts() {
+        this.goods = [
+            { id: 1, title: 'Notebook', price: 2000 },
+            { id: 2, title: 'Mouse', price: 20 },
+            { id: 3, title: 'Keyboard', price: 200 },
+            { id: 4, title: 'Gamepad', price: 50 },
+        ];
+    }
 
-const products = [
-    { id: 1, title: 'Notebook', price: 2000 },
-    { id: 2, title: 'Mouse', price: 20 },
-    { id: 3, title: 'Keyboard', price: 200 },
-    { id: 4, title: 'Gamepad', price: 50 },
-];
+    render() {
+        const block = document.querySelector(this.container);
+        for (let product of this.goods) {
+            const item = new ProductItem(product);
+            block.insertAdjacentHTML("beforeend", item.render());
+        }
+    }
+}
 
-
-const renderProduct = item => {
-    return `<div class="product-item">
-    <img src="${defaultImage}" alt="карточка продукта" class="product-item__image">
-                <h3>${item.title}</h3>
-                <p>${item.price}</p>
+class ProductItem {
+    constructor(product, img = 'https://santehmag.com.ua/image/cache/catalog/import_yml/SD0/003/724/6/no-img-200x200.jpg') {
+        this.title = product.title;
+        this.id = product.id;
+        this.price = product.price;
+        this.img = img;
+    }
+    render() {
+        return `<div class="product-item">
+    <img src="${this.img}" alt="карточка продукта" class="product-item__image">
+                <h3>${this.title}</h3>
+                <p>${this.price}</p>
                 <button class="buy-btn">
                 <span></span>
                 Купить
                 </button>
             </div>`
+    }
+}
 
-};
-const renderPage = (list = []) => {
-    list.forEach(element => {
-        document.querySelector('.products').insertAdjacentHTML('beforeend', renderProduct(element))
-    })
-};
-
-
-//переписал renderPage так, чтобы исключить появление запятой. 
-//метод innerHtml брал элементы массива с версткой, включая запятые массива, добавлял разметку на страницу.
-//можно было бы привести массив к строке с помощью метода join(указать разделитель '') затем через innerHtml поместить код на страницу
-renderPage(products);
+let list = new ProductList();
